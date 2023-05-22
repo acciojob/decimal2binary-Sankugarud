650ae463e17e602931dfe53d51fc3bc8b707ad7d
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-05-23 01:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function decimalToBinary(decimal) {
-  return Number.parseInt(decimal, 2); 
+  if (decimal === 0) {
+    return '0'; // Special case for decimal number 0
+  }
+	let temp = 1;
+  let binary = 0;
+	let bi = 0;
+  while (decimal > 0) {
+	  binary = (decimal % 2) ;
+	  bi = bi + (binary*temp);
+      decimal = parseInt(decimal / 2);
+	  temp = temp*10;
+  }
+
+  return bi;
 }
-console.log(decimalToBinary(7));
+console.log(decimalToBinary(7))
+// Example us
 
 console.log(`Decimal: ${decimalNumber}`);
 console.log(`Binary: ${binaryNumber}`);
